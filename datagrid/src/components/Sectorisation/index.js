fb909e8e54a9c2cc2e2e0357f537623b13ec41e9
Alexandre Menskoï <a.menskoi@gmail.com>
2022-04-22 14:51:48 +0200

    Creating base project and organizing files

---
+++ datagrid/src/components/Sectorisation/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import App from "../../containers/App";
+import sectorisation from "json!../../assets/Sectorisation.json";
+
+export const Sectorisation = () => {
+    return (
+        <>
+            <div className="sectorisation-container">
+                
+            </div>
+        </>
+    );
+}
---
+++ datagrid/src/containers/App/index.js
@@ -0,0 +1,13 @@
+import logo from '../../assets/logo.svg';
+import './App.css';
+import { Sectorisation } from '../../components/Sectorisation';
+
+function App() {
+  return (
+    <div className="App">
+      <Sectorisation />
+    </div>
+  );
+}
+
+export default App;
