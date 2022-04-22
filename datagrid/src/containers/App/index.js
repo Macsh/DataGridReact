3b99153eb9b8b4eed05e4d2fd84fa40ee827a8f9
Alexandre Menskoï <a.menskoi@gmail.com>
2022-04-22 16:34:44 +0200

    Tree working

--- datagrid/src/containers/App/index.js
+++ datagrid/src/containers/App/index.js
@@ -1,11 +1,11 @@
-import logo from '../../assets/logo.svg';
 import './App.css';
+import datas from "../../assets/Sectorisation.json";
 import { Sectorisation } from '../../components/Sectorisation';
 
 function App() {
   return (
     <div className="App">
-      <Sectorisation />
+      <Sectorisation datas={datas.data.roots} />
     </div>
   );
 }
